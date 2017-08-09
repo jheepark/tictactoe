0bbc8fb933b774b97ed4a819d8af6b10c34223eb
Jae-Hee Park <jhee.park.work@gmail.com>
2017-08-09 12:55:27 +1000

    console log tictactoe

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 console.log("%cHello and welcome to my Tic Tac Toe!", "background: rgba(136, 4, 247, 0.87);color:#FFF;padding:5px;border-radius: 5px;line-height: 26px;");
 
+
+console.log ("%c_X_|_O_|_X_","background: rgb(246, 48, 208);color:#FFF;padding:2px;border-radius: 5px;line-height: 10px;");
+console.log ("%c_O_|_X_|_X_", "background: rgb(246, 48, 208);color:#FFF;padding:2px;border-radius: 5px;line-height: 10px;");
+console.log ("%c_O_|_X_|_O_", "background: rgb(246, 48, 208);color:#FFF;padding:2px;border-radius: 5px;line-height: 10px;");
+
 const checkClass = function(){
   if (logic1() || logic2() || logic3() || logic4() || logic5() || logic6() || logic7() || logic8()) {
   $('.Win').show(500).append("<span class='winmessage'>X Wins</span>");
